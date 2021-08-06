72215af151e001af91a6bf4570c0386158b4d4ec
Andrew Giang <giang184@gmail.com>
2021-08-06 15:27:48 -0700

    add validation for rating

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,8 @@ class Review < ApplicationRecord
   validates :content_body, presence: true
   validates :rating, presence: true
   validates_length_of :content_body, maximum: 250
+  validates_numericality_of :rating, less_than_or_equal_to: 5
+  validates_numericality_of :rating, greater_than_or_equal_to: 0
   before_save(:titleize_review)
 
   private
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -6,4 +6,6 @@ describe Review do
   it { should validate_presence_of :content_body }
   it { should validate_presence_of :rating }
   it { should validate_length_of(:content_body).is_at_most(250) }
+  it { should validate_numericality_of(:rating).is_less_than_or_equal_to(5) }
+  it { should validate_numericality_of(:rating).is_greater_than_or_equal_to(0) }
 end
