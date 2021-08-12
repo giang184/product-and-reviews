a087adea3f4687b98a012b015fd25199f20d7125
Andrew Giang <giang184@gmail.com>
2021-08-11 17:01:59 -0700

    add more integration test for product

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -5,7 +5,7 @@ describe "the add a product process" do
     visit products_path
     click_link 'Add new product'
     fill_in 'Name', :with => 'Coors Light'
-    fill_in 'Cost', :with => '12.95'
+    fill_in 'Cost', :with => '5.95'
     fill_in 'Country of origin', :with => 'American'
     click_on 'Create Product'
     expect(page).to have_content 'Product successfully added!'
@@ -17,4 +17,23 @@ describe "the add a product process" do
     click_on 'Create Product'
     expect(page).to have_content "Name can't be blank"
   end
+
+  it "edit a new product" do
+    visit products_path
+    click_link 'Add new product'
+    fill_in 'Name', :with => 'Blue Label'
+    fill_in 'Cost', :with => '7.95'
+    fill_in 'Country of origin', :with => 'Mexican'
+    click_on 'Create Product'
+    within('.all_product') do
+      click_on 'Blue Label'
+    end
+    click_on 'Edit'
+    fill_in 'Name', :with => 'Black Label'
+    fill_in 'Cost', :with => '3.95'
+    fill_in 'Country of origin', :with => 'Mexican'
+    click_on 'Update Product'
+    expect(page).to have_content 'Black Label'
+  end
+
 end
