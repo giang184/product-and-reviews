e807ef3adfe43f954a09ad303d67f00b1e4b5c08
Andrew Giang <giang184@gmail.com>
2021-08-13 16:46:06 -0700

    add more integration tests to get to 87.25% covered

--- spec/features/add_product_pages_spec.rb
+++ spec/features/add_product_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
-describe "the add a product process" do
+describe "Integration Test: " do
+
   it "should not add product" do
     visit products_path
     expect(page).not_to have_content 'Add new product'
@@ -32,22 +33,81 @@ describe "the add a product process" do
     expect(page).to have_content 'a@a.com'
   end
 
-  # it "edit a new product" do
-  #   visit products_path
-  #   click_link 'Add new product'
-  #   fill_in 'Name', :with => 'Blue Label'
-  #   fill_in 'Cost', :with => '7.95'
-  #   fill_in 'Country of origin', :with => 'Mexican'
-  #   click_on 'Create Product'
-  #   # within('.all_product') do
-  #   #   click_on 'Blue Label'
-  #   # end
-  #   click_on 'Edit'
-  #   fill_in 'Name', :with => 'Black Label'
-  #   fill_in 'Cost', :with => '3.95'
-  #   fill_in 'Country of origin', :with => 'Mexican'
-  #   click_on 'Update Product'
-  #   expect(page).to have_content 'Black Label'
-  # end
+  it "Add a new product" do
+    User.create(:email => "admin@a.com", :password => "a", admin: true)
+    visit signin_path
+    fill_in 'Email', :with => 'admin@a.com'
+    fill_in 'Password', :with => 'a'
+    click_on 'Sign in!'
+    visit products_path
+    click_on 'Add a new product'
+    fill_in 'Name', :with => 'Blue Label'
+    fill_in 'Cost', :with => '7.95'
+    fill_in 'Country of origin', :with => 'Mexican'
+    click_on 'Create Product'
+    expect(page).to have_content 'Blue Label'
+  end
 
+  it "Edit a new product" do
+    User.create(:email => "admin@a.com", :password => "a", admin: true)
+    visit signin_path
+    fill_in 'Email', :with => 'admin@a.com'
+    fill_in 'Password', :with => 'a'
+    click_on 'Sign in!'
+    visit products_path
+    click_on 'Add a new product'
+    fill_in 'Name', :with => 'Black Label'
+    fill_in 'Cost', :with => '7.95'
+    fill_in 'Country of origin', :with => 'Mexican'
+    click_on 'Create Product'
+    click_on 'Edit product'
+    fill_in 'Name', :with => 'Red Label'
+    fill_in 'Cost', :with => '4.95'
+    fill_in 'Country of origin', :with => 'Mexican'
+    click_on 'Update Product'
+    expect(page).to have_content 'Red Label'
+  end
+  
+  it "should allow user to add review" do
+    User.create(:email => "admin@a.com", :password => "a", admin: true)
+    User.create(:email => "bab@a.com", :password => "a", admin: false)
+    visit signin_path
+    fill_in 'Email', :with => 'admin@a.com'
+    fill_in 'Password', :with => 'a'
+    click_on 'Sign in!'
+    visit products_path
+    click_on 'Add a new product'
+    fill_in 'Name', :with => 'Blue Label'
+    fill_in 'Cost', :with => '7.95'
+    fill_in 'Country of origin', :with => 'Mexican'
+    click_on 'Create Product'
+    click_on 'Sign out'
+    visit signin_path
+    fill_in 'Email', :with => 'bab@a.com'
+    fill_in 'Password', :with => 'a'
+    click_on 'Sign in!'
+    visit products_path
+    click_on 'Blue Label'
+    click_on 'Add a review'
+    expect(page).to have_content 'New Review'
+  end
+
+  it "should not allow none user to add review" do
+    User.create(:email => "admin@a.com", :password => "a", admin: true)
+    visit signin_path
+    fill_in 'Email', :with => 'admin@a.com'
+    fill_in 'Password', :with => 'a'
+    click_on 'Sign in!'
+    visit products_path
+    click_on 'Add a new product'
+    fill_in 'Name', :with => 'Blue Label'
+    fill_in 'Cost', :with => '7.95'
+    fill_in 'Country of origin', :with => 'Mexican'
+    click_on 'Create Product'
+    click_on 'Sign out'
+    visit products_path
+    click_on 'Blue Label'
+    click_on 'Add a review'
+    expect(page).to have_content 'Password'
+  end
 end
